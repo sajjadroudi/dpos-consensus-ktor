3cf2003f31d391c5ba459ae550103269388ef525
roudi <sajjad.roodi@gmail.com>
2024-01-15 11:38:16 +0330

    Build genesis block

--- src/main/kotlin/roudi/ir/blockchain/BlockChain.kt
+++ src/main/kotlin/roudi/ir/blockchain/BlockChain.kt
@@ -2,10 +2,17 @@ package roudi.ir.blockchain
 
 class BlockChain {
 
-    private val blocks = mutableListOf<Block>()
+    private val blocks = mutableListOf<Block>(buildGenesisBlock())
 
     fun addBlock(block: Block) {
         blocks += block
+    }
+
+    private fun buildGenesisBlock(): Block {
+        return Block(
+            transactions = emptyList(),
+            0
+        )
     }
 
 }
