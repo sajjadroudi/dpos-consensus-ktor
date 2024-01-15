e46348e258809a1e496f7ff95f45d39cbfccb6f1
roudi <sajjad.roodi@gmail.com>
2024-01-15 12:00:45 +0330

    Implement Node.addNode()

--- src/main/kotlin/roudi/ir/node/Node.kt
+++ src/main/kotlin/roudi/ir/node/Node.kt
@@ -26,4 +26,8 @@ class Node {
         unverifiedTransactions += transaction
     }
 
+    fun addNode(node: NodeInfo) {
+        nodes += node
+    }
+
 }
--- src/main/kotlin/roudi/ir/node/NodeInfo.kt
+++ src/main/kotlin/roudi/ir/node/NodeInfo.kt
@@ -2,8 +2,10 @@ package roudi.ir.node
 
 data class NodeInfo(
     val address: String,
-    private val stake: Int,
-    private val voteCount: Int
+    private val stake: Int
 ) {
+
+    var voteCount = 0
+
     val power = stake * voteCount
 }
