cc6cbfa377cb0f0030aa56c3cb4d36d67dcd2621
roudi <sajjad.roodi@gmail.com>
2024-01-25 23:24:09 +0330

    Add PopulateTransaction script

--- src/main/kotlin/roudi/ir/script/PopulateTransaction.kt
+++ src/main/kotlin/roudi/ir/script/PopulateTransaction.kt
@@ -1,33 +1,38 @@
 package roudi.ir.script
 
-import io.ktor.client.*
-import io.ktor.client.engine.cio.*
 import io.ktor.client.request.*
 import kotlinx.coroutines.runBlocking
 import roudi.ir.Config
 import roudi.ir.blockchain.Transaction
 import roudi.ir.blockchain.toTransactionRequest
+import roudi.ir.util.KtorClientBuilder
 
-private val names = listOf("John", "Jack", "Bill",
+private val names = listOf(
+    "John", "Jack", "Bill",
     "Tim", "Sophia", "Alice",
-    "Donald", "Brad", "Juliet", "Ana")
+    "Donald", "Brad", "Juliet", "Ana",
+    "Jin", "Pam", "Michele", "Kevin",
+    "Sam", "Mosh", "Edvin", "Wayne", "Gary",
+    "Emmy", "Scarlett", "Lana", "James", "Adele"
+)
 
 fun main(): Unit = runBlocking {
-    val client = HttpClient(CIO)
+    val client = KtorClientBuilder.build()
     (1 until Config.NODE_COUNT).map {
         val port = Config.PRIMARY_NODE_PORT + it
-        val url = "http://${Config.HOST}:$port"
+        val url = "http://${Config.HOST}:$port/transaction"
 
         val transactionCount = (2..5).random()
         repeat(transactionCount) {
-            client.post(url) {
-                setBody(buildTransaction().toTransactionRequest())
+            val response = client.post(url) {
+                val request = buildTransaction().toTransactionRequest()
+                setBody(request)
             }
         }
     }
 }
 
-private fun buildTransaction() : Transaction {
+private fun buildTransaction(): Transaction {
     return Transaction(
         names.random(),
         names.random(),
