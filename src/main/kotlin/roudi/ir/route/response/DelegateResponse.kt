0e6066968bd7598c775618b4fc06066d6e0d9557
roudi <sajjad.roodi@gmail.com>
2024-01-25 18:18:37 +0330

    Create DelegateResponse class

---
+++ src/main/kotlin/roudi/ir/route/response/DelegateResponse.kt
@@ -0,0 +1,12 @@
+package roudi.ir.route.response
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class DelegateResponse(
+    @SerialName("address") val address: String,
+    @SerialName("coin") val coin: Int,
+    @SerialName("stake") val stake: Int,
+    @SerialName("power") val power: Int
+)
